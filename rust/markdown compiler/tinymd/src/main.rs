9782a9e345527da89cf264fafd3ca613e6e9a54c
Smaran Harihar <smaran.harihar@gmail.com>
2020-05-17 09:29:32 -0700

    chap 2

--- rust/markdown compiler/tinymd/src/main.rs
+++ rust/markdown compiler/tinymd/src/main.rs
@@ -1,3 +1,13 @@
+fn usage() {
+    let version: u16 = get_version();
+    println!("tinymd is a markdown complier written by Sam");
+    println!("version number is {}", version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
+}
+
+fn get_version() -> u16 {
+    1000
 }
