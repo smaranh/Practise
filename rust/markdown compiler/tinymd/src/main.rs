7b56e76619d60a819061db8716a043a27b9c8600
Smaran Harihar <smaran.harihar@gmail.com>
2020-05-19 19:45:29 -0700

    Chap 3

--- rust/markdown compiler/tinymd/src/main.rs
+++ rust/markdown compiler/tinymd/src/main.rs
@@ -1,13 +1,34 @@
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("),");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    return the_title;
+}
+
+fn parse_markdown_file() {
+
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    let mut the_title = String::from("\nWritten by: ");
+    the_title.push_str(env!("CARGO_PKG_AUTHORS"));
+    the_title.push_str("\nHomepage: ");
+    the_title.push_str(env!("CARGO_PKG_HOMEPAGE"));
+    the_title.push_str("\nUsage: tinymd <somefile>.md");
+    println!("{}", the_title);
+}
+
 fn usage() {
-    let version: u16 = get_version();
-    println!("tinymd is a markdown complier written by Sam");
-    println!("version number is {}", version);
+    print_long_banner();
 }
 
 fn main() {
     usage();
-}
-
-fn get_version() -> u16 {
-    1000
 }
