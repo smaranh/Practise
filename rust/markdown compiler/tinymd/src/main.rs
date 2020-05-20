dede960b7f006d718826cad3145d0679c19d7c4e
Smaran Harihar <smaran.harihar@gmail.com>
2020-05-20 10:26:22 -0700

    chap 4

--- rust/markdown compiler/tinymd/src/main.rs
+++ rust/markdown compiler/tinymd/src/main.rs
@@ -2,13 +2,14 @@ fn get_title() -> String {
     let mut the_title = String::from(env!("CARGO_PKG_NAME"));
     the_title.push_str(" (v");
     the_title.push_str(env!("CARGO_PKG_VERSION"));
-    the_title.push_str("),");
+    the_title.push_str("),\n");
     the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
     return the_title;
 }
 
-fn parse_markdown_file() {
-
+fn parse_markdown_file(_file: &str) {
+    print_short_banner();
+    println!("Parsing your file {}...", _file);
 }
 
 fn print_short_banner() {
@@ -30,5 +31,14 @@ fn usage() {
 }
 
 fn main() {
-    usage();
+    // Collect all arguments in a vector
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => parse_markdown_file(&args[1]),
+        _ => {
+            println!("You forgot to provide a file");
+            usage();
+        }
+    }
 }
