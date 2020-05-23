b4b095a523fca6e26729ab44399d4a64d9a78465
Smaran Harihar <smaran.harihar@gmail.com>
2020-05-22 19:24:09 -0700

    add pause fn

--- rust/guessing game/guessgame/src/main.rs
+++ rust/guessing game/guessgame/src/main.rs
@@ -1,6 +1,23 @@
 use std::io;
+use std::io::prelude::{Write, Read};
 use std::cmp::Ordering;
 use rand::Rng;
+
+// To pause at the end of program. Useful if running a Windows
+// exe. Without pause the command line would exit without
+// displaying the result
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line,
+    // so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+}
 
 fn main() {
     println!("Lets play a guessing game");
@@ -22,6 +39,7 @@ fn main() {
             Ordering::Greater => println!("Your guess is high"),
             Ordering::Equal => {
                 println!("You WIN!!");
+                pause();
                 break;
             }
         }
